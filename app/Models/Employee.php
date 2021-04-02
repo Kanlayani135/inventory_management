e9f581fa22955b4c70ea230da8c11b3ca2f86404
Kanlayani135 <kanlayani_b@cmu.ac.th>
2021-04-02 12:42:28 +0700

    fdsfe

---
+++ app/Models/Employee.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    use HasFactory;
+    protected $fillable =['code','fname','lname','address','sex','dob','age','tel','civilstatus','position','workstatus','hireddate'];
+    function departments() {
+        return $this->belongto(Department::class);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use App\Http\Controllers\HomeController;
 
 
 Auth::routes();
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('admin/home',[HomeController::class, 'adminHome'])->name('admin.home')->middleware('is_admin');
