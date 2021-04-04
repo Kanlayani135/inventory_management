5f5f2149f70dc94cbb306f4178fb435428c92afa
Thawanrat Tanbud <twrtb9597@gmail.com>
2021-04-04 22:43:45 +0700

    fghjkl;'

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -11,16 +11,9 @@ class Employee extends Model
     protected $fillable =['code','fname','lname','address','sex','dob','age','tel','civilstatus','position','department_id','division_id','workstatus','hireddate'];
     
     function departments() {
-        return $this->belongto(Department::class);
-<<<<<<< HEAD
-=======
-
-        function divisions() {
-            return $this->belongto(Division::class);
->>>>>>> 1f435c702ac20abcba15137c96f91c4f176ffdda
+        return $this->belongsto(Department::class);
     }
-
     function divisions() {
-        return $this->belongto(Division::class);
+        return $this->belongsto(Division::class);
     }
 }
