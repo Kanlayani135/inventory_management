16519dd180971f001912a6d1a4af050fe454c67f
Thawanrat Tanbud <twrtb9597@gmail.com>
2021-04-04 16:04:45 +0700

    nm,./

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,9 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
-    protected $fillable =['code','fname','lname','address','sex','dob','age','tel','civilstatus','position','department_id','department','division_id','division','workstatus','hireddate'];
+    protected $fillable =['code','fname','lname','address','sex','dob','age','tel','civilstatus','position','department','division','workstatus','hireddate','department_id','division_id'];
     
     function departments() {
         return $this->belongto(Department::class);
+    }
+
+    function division() {
+        return $this->belongto(Division::class);
     }
 }
